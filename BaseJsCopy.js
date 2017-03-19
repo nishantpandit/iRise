32e8efe0e9a82f3f9495bb1d8d44579edf38c0af
Nishant Pandit <nishant.pandit@vocado.com>
2017-03-19 09:01:27 -1000

    Updated templates to match new schema 03192017

--- BaseJsCopy.js
+++ BaseJsCopy.js
@@ -1,5 +1,5 @@
 var documentConfiguration = {
-  var moduleName = 
+  var moduleName = "Application Registration"
   //docTitle: "Detailed Design Specification Document",
   //docSubject: "", // if blank, iRise project name is used
   //docDescription: "",
@@ -26,99 +26,160 @@ var documentConfiguration = {
     page_break: "after"
   }, 
   {
+    sectionName: "Purpose",
+    //includeTitle: "h1",
+    getData: "requirements",
+    filters: [
+      function(requirement) {
+        return requirement["Details-Type"] == "Purpose" && requirement["Details-Module Name"] == moduleName;
+      }
+    ],
+    template: "Purpose_template",
+    page_break: "after"
+  },
+  {
+    sectionName: "Requirement",
+    //includeTitle: "h1",
+    getData: "requirements",
+    filters: [
+      function(requirement) {
+        return requirement["Details-Type"] == "Requirement" && requirement["Details-Module Name"] == moduleName;
+      }
+    ],
+    template: "Requirement_template",
+    page_break: "after"
+  },
+  {
+    sectionName: "Track",
+    //includeTitle: "h1",
+    getData: "requirements",
+    filters: [
+      function(requirement) {
+        return requirement["Details-Type"] == "Track" && requirement["Details-Module Name"] == moduleName;
+      }
+    ],
+    template: "Track_template",
+    page_break: "after"
+  },
+
+  {
     sectionName: "Narrative",
     //includeTitle: "h1",
     getData: "requirements",
     filters: [
       function(requirement) {
-        return requirement["Details-Type"] == "Narrative" && requirement["Details-Module Name"] == "Application Registration";
+        return requirement["Details-Type"] == "Narrative" && requirement["Details-Module Name"] == moduleName;
       }
     ],
     template: "Narrative_template",
     page_break: "after"
   },
   {
-    sectionName: "Save Checkpoint List",
+    sectionName: "Screen Flow Summary",
     //includeTitle: "h1",
     getData: "requirements",
     filters: [
       function(requirement) {
-        return requirement["Details-Type"] == "Save Checkpoints" && requirement["Details-Module Name"] == "Application Registration";
+        return requirement["Details-Type"] == "Screen Flow Summary" && requirement["Details-Module Name"] == moduleName;
       }
     ],
-    template: "SaveCheckpoints_template",
+    template: "ScreenFlowSummary_template",
+    page_break: "after"
+  },
+  {
+    sectionName: "Save Checkpoints",
+    //includeTitle: "h1",
+    getData: "requirements",
+    filters: [
+      function(requirement) {
+        return requirement["Details-Type"] == "Save Checkpoint" && requirement["Details-Module Name"] == moduleName;
+      }
+    ],
+    template: "SaveCheckpoint_template",
+    page_break: "after"
+  }, 
+  {
+    sectionName: "Save Display Conditions",
+    //includeTitle: "h1",
+    getData: "requirements",
+    filters: [
+      function(requirement) {
+        return requirement["Details-Type"] == "Save Display Condition" && requirement["Details-Module Name"] == moduleName;
+      }
+    ],
+    template: "SaveDisplayCondition_template",
+    page_break: "after"
+  }, 
+  {
+    sectionName: "Assumptions",
+    //includeTitle: "h1",
+    getData: "requirements",
+    filters: [
+      function(requirement) {
+        return requirement["Details-Type"] == "Assumption" && requirement["Details-Module Name"] == moduleName;
+      }
+    ],
+    template: "Assumption_template",
     page_break: "after"
   }, 
     {
-    sectionName: "Business Scenario List",
+    sectionName: "Business Scenarios",
     //includeTitle: "h1",
     getData: "requirements",
     filters: [
       function(requirement) {
-        return requirement["Details-Type"] == "Business Scenario" && requirement["Details-Module Name"] == "Application Registration";
+        return requirement["Details-Type"] == "Business Scenario" && requirement["Details-Module Name"] == moduleName;
       }
     ],
     template: "BusinessScenario_template",
     page_break: "after"
   }, 
+  {
+    sectionName: "Main Flows",
+    //includeName: "h1",
+    getData: "requirements",
+    filters: [
+      function(requirement) {
+        return requirement["Details-Type"] == "Main Flow Step" && requirement["Details-Module Name"] == moduleName;
+      }
+    ],
+    template: "MainFlow_template",
+    page_break: "after"
+  }, 
+  {
+    sectionName: "Field Descriptions",
+    //includeName: "h1",
+    getData: "requirements",
+    filters: [
+      function(requirement) {
+        return requirement["Details-Type"] == "Field Description" && requirement["Details-Module Name"] == moduleName;
+      }
+    ],
+    template: "FieldDescription_template",
+    page_break: "after"
+  },
+  {
+    sectionName: "Referenced Documents",
+    //includeName: "h1",
+    getData: "requirements",
+    filters: [
+      function(requirement) {
+        return requirement["Details-Type"] == "Referenced Document" && requirement["Details-Module Name"] == moduleName;
+      }
+    ],
+    template: "ReferencedDocument_template",
+    page_break: "after"
+  },
     {
     sectionName: "Screen Designs",
     //includeTitle: "h1",
     getData: "screens",
     filters: [
       function(screen) {
-        return screen["type"] == "PAGE";
+        return screen["type"] == "PAGE" && requirement["Details-Module Name"] == moduleName;
       }
     ],
     template: "ScreenDesigns_template",
     page_break: "after"
-  }, 
-    {
-    sectionName: "Business Rules",
-    //includeTitle: "h1",
-    getData: "requirements",
-    filters: [
-      function(requirement) {
-        return requirement["Details-Type"] == "Business Rule";
-      }
-    ],
-    template: "Business_Rules_template",
-    page_break: ""
-  }, 
-    {
-    sectionName: "Non-UI Content",
-    //includeName: "h1",
-    getData: "requirements",
-    filters: [
-      function(requirement) {
-        return requirement["Details-Type"] == "Non-UI Content";
-      }
-    ],
-    template: "NonUI_template",
-    page_break: "after"
-  }, 
-    {
-    sectionName: "Main Flows",
-    //includeName: "h1",
-    getData: "requirements",
-    filters: [
-      function(requirement) {
-        return requirement["Details-Type"] == "Main Flow Step";
-      }
-    ],
-    template: "MainFlow_template",
-    page_break: "after"
-  }, 
-    {
-    sectionName: "Reference Tables",
-    //includeName: "h1",
-    getData: "requirements",
-    filters: [
-      function(requirement) {
-        return requirement["Details-Type"] == "Reference Table";
-      }
-    ],
-    template: "NonUI_template",
-    page_break: ""
   }]
 };
